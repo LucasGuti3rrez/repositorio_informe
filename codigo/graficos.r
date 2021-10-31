3fb3ae039447bf9cbdba17ca41ade181ef2a1b01
Lucas Gutierrez <lucas.gutirrez@uc.cl>
2021-10-30 22:07:14 -0300

    graficos

---
+++ codigo/graficos.r
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(ggplot2)
+
+espectadores = read_delim("../datos/espectadores.csv")
+goles = read_delim("../datos/goles.csv")
+
+
+Goles = goles$Goles
+AsisTotal = espectadores$`Asistencia total`
+Mundiales = NULL
+Mundiales$Sede = espectadores$Sede
+Mundiales$Asisttotal = espectadores$`Asistencia total`
+Mundiales$`Media de asistencia por partido` = espectadores$`Media de asistencia por partido`
+Mundiales$Goles = goles$Goles
+Mundiales = as.data.frame(Mundiales)
+
+ggplot(Mundiales,aes(x = Asisttotal/1000, y = Goles), lwd = 2) +
+  geom_point() +
+  labs(title = 'Goles en funcion de la Asistencia',
+       x = 'Asistencia Total (miles)',
+       y = 'Goles')
+ggsave("../figuras/dispersion.png")
+
+regresion = lm(Goles ~ AsisTotal)
+
+ggplot(Mundiales,aes(x = Asisttotal, y = Goles), lwd = 2) +
+  geom_point() +
+  labs(title = 'Goles en funcion de la Asistencia',
+       x = 'Asistencia Total',
+        y = 'Goles') +
+  geom_abline(intercept = regresion$coefficients[1] , slope = regresion$coefficients[2], col = 2)
+ggsave("../figuras/regresion.png")
+
+
+getwd()
