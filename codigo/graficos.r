5bf3ab08a088602a8b7e67828a8868171c18c8a2
Lucas Gutierrez <lucas.gutirrez@uc.cl>
2021-11-08 12:31:18 -0300

    acitivdad 11

--- codigo/graficos.r
+++ codigo/graficos.r
@@ -4,7 +4,6 @@ library(ggplot2)
 espectadores = read_delim("../datos/espectadores.csv")
 goles = read_delim("../datos/goles.csv")
 
-
 Goles = goles$Goles
 AsisTotal = espectadores$`Asistencia total`
 Mundiales = NULL
@@ -17,8 +16,9 @@ Mundiales = as.data.frame(Mundiales)
 ggplot(Mundiales,aes(x = Asistotal/1000, y = Goles), lwd = 2) +
   geom_point() +
   labs(title = 'Goles en funcion de la Asistencia',
+       subtitle = 'En todos los mundiales (1930-2018)',
        x = 'Asistencia Total (miles)',
-       y = 'Goles')
+       y = 'Goles Totales')
 ggsave("../figuras/dispersion.png")
 
 regresion = lm(Goles ~ AsisTotal)
@@ -26,8 +26,10 @@ regresion = lm(Goles ~ AsisTotal)
 ggplot(Mundiales,aes(x = Asistotal, y = Goles), lwd = 2) +
   geom_point() +
   labs(title = 'Goles en funcion de la Asistencia',
-       x = 'Asistencia Total',
-        y = 'Goles') +
+       subtitle = 'En todos los mundiales (1930-2018)',
+       x = 'Asistencia Total (miles)',
+        y = 'Goles Totales') +
+  scale_x_continuous(breaks = c(0,10^6,2*10^6,3*10^6,4*10^6),
+                     labels = c(0,"1000","2000","3000","4000")) +
   geom_abline(intercept = regresion$coefficients[1] , slope = regresion$coefficients[2], col = 2)
 ggsave("../figuras/regresion.png")
-
